542947180be0a59fd0ff22ecee8b32ce723d0dc5
Ben Macaulay <ben.macaulay@gmail.com>
2023-12-07 09:35:16 +1300

    Added metadata to compiled exe
    -Description 'https://github.com/ben-macaulay/run' -Company 'GPLv3' -product Run

--- compile.cmd
+++ compile.cmd
@@ -1,4 +1,4 @@
-powershell.exe -exec bypass -nop -c "ps2exe -inputFile %~dp0run.ps1 -outputFile %~dp0Run.exe -iconFile %~dp0cog.ico -noConsole -title "RunV3" -version 0.0.3.4 -x86"
+powershell.exe -exec bypass -nop -c "ps2exe -inputFile %~dp0run.ps1 -outputFile %~dp0Run.exe -iconFile %~dp0cog.ico -noConsole -title RunV3 -version 0.0.3.4 -x86 -Description 'https://github.com/ben-macaulay/run' -Company 'GPLv3' -product Run"
 @ping 127.0.0.1 -n 3 >nul
 :: -noerror
 :: pause
